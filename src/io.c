3309a5d922997be5e536c42c9f38584b652bf481
mjolk <2mjolk@gmail.com>
2019-03-27 22:09:50 +0100

    started on io layer

--- src/io.c
+++ src/io.c
@@ -5,34 +5,33 @@
  * Date   : ma 11 feb 2019 12:17
  */
 
+#include <sys/socket.h>
 #include <stdio.h>
-#include "node_io.h"
-#include "message.h"
+#include "io.h"
+#include "llrb-interval/slist.h"
+#include "llrb-interval/llrb-interval.h"
 #define MAX_LATENCY 60
 #define PREFIX_SIZE 4
-#define N 3
 
-void internal_in(struct connection*, struct node_io*, int rep);
-void internal_out(struct node_io*, int rep);
-
-int report(int ch, int *rc){
-    return chsend(ch, rc, sizeof(int), 20);
+int spcmp(struct registered_span *sp1, struct registered_span *sp2){
+    return strcmp(sp1->start_key, sp2->start_key);
 }
 
-void reset_connection(struct connection *c){
-    pthread_mutex_lock(&c->lock);
-    c->alive = 0;
-    hclose(c->ap);
-    pthread_mutex_unlock(&c->lock);
-}
+SLL_HEAD(client_group, span);
+LLRB_GENERATE(client_tree, registered_span, entry, spcmp);
+LLRB_RANGE_GROUP_GEN(client_tree, registered_span, entry, client_group, next);
+
+void step_writer(struct connection*);
+void step_reader(struct connection*);
+
 //does this work, need extra pointer indirect/return new array?
-void update_quorum(size_t *quorum, struct connection *c){
+void update_quorum(size_t *quorum, struct connection *ns){
     for(int ci = 0;ci < N;ci++){
-        struct connection *con = &c[ci];
+        struct connection reader = ns[ci];
         for(int qi = 0; qi < N; qi++){
-            if(con->latency < c[quorum[qi]].latency &&
-                    con->latency > c[quorum[qi -1]].latency){
-                quorum[qi] = con->id;
+            if(reader.latency < ns[quorum[qi]].latency &&
+                    reader.latency > ns[quorum[qi -1]].latency){
+                quorum[qi] = reader.xreplica_id;
             }
         }
     }
@@ -46,48 +45,135 @@ int expired(struct connection *c){
     return (c->fbs.deadline < now());
 }
 
-int connect(int rep, struct node_io *n, size_t xrid){
-    struct connection *c = &n->cons[xrid];
-    if(slow(c)) return -1;
-    pthread_mutex_lock(&c->lock);
-    hclose(c->handle);
-    c->alive = 0;
-    hclose(c->ap);
-    c->handle = tcp_connect(&c->addr, -1);
-    if(c->handle < 0) return -1;
-    c->handle = fbs_sock_attach(c->handle, &c->fbs);
-    if(c->handle < 0) return -1;
-    if(c->fbs.xreplica_id != xrid) return -1;
-    c->id = c->fbs.xreplica_id;
-    if(c->p == TLS){
+int run_conn(struct connection *c){
+    c->status = ALIVE;
+    bundle_go(c->ap, step_writer(c));
+    bundle_go(c->ap, step_reader(c));
+    return 0;
+};
 
-    }
-    c->alive = 1;
-    bundle_go(c->ap, internal_in(c, n, rep));
-    pthread_mutex_unlock(&c->lock);
-    return (c->handle >= 0)?0:-1;
-}
-
-int listen(int port) {
+//TODO TLS/UDP
+int listener(int port) {
     struct ipaddr addr;
     ipaddr_local(&addr, NULL, port, 0);
     return tcp_listen(&addr, 10);
 }
 
-static void set_con_state(struct fbs_sock *f, size_t r_id){
-    f->replica_id = r_id;
-    f->hdrlen = 4;
-    f->deadline = 80;
-    f->encode = message_to_buffer;
-    f->decode = message_from_buffer;
+void fd_close(int fd){
+    struct linger lng;
+    lng.l_onoff=1;
+    lng.l_linger=0;
+    setsockopt(fd, SOL_SOCKET, SO_LINGER, (void*)&lng, sizeof(lng));
+    /* We are not checking the error here. close() has inconsistent behaviour
+       and leaking a file descriptor is better than crashing the entire
+       program. */
+    close(fd);
+}
+
+int new_connection(struct connection *c){
+    if(slow(c)) return -1;
+    c->status = CONNECTING;
+    c->fbs.deadline = 80;
+    c->fbs.hdrlen = 4;
+    c->fbs.decode = message_from_buffer;
+    c->fbs.encode = message_to_buffer;
+    chan_init(&c->chan_read);
+    if(c->fd > 0){
+        c->handle = tcp_fromfd(c->fd);
+    } else {
+        c->handle = tcp_connect(&c->addr, now() + 160);
+    }
+    c->handle = fbs_sock_attach(c->handle, &c->fbs);
+    if(c->handle < 0 && c->fd > 0) {
+        goto release_fd;
+    } else if(c->handle < 0){
+        goto release_handle;
+    }
+    size_t xreplica_id = fbs_sock_remote_id(c->handle);
+    c->xreplica_id = xreplica_id;
+    run_conn(c);
+    return 0;
+release_handle:
+    hclose(c->handle);
+    return -1;
+release_fd:
+    fd_close(c->fd);
+    return -1;
+}
+
+void update_nodes(struct connection *c){
+    //TODO locking
+    struct connection oc = c->n->chan_nodes[c->xreplica_id];
+    if(oc.status != DEAD){
+        pthread_cancel(*oc.tid);
+    }
+    memcpy(&c->n->chan_nodes[c->xreplica_id], c, sizeof(struct connection));
+}
+
+void *try_connect(void *conn){
+    struct connection *c = conn;
+    int tries = 0;
+    int reconnect = 3;
+    int rc = 0;
+    do {
+        rc = new_connection(c);
+        tries++;
+        if(rc < 0){
+            sleep(c->back_off*tries);
+            continue;
+        } else {
+            tries = 0;
+            //TODO sync
+            update_nodes(c);
+            bundle_wait(c->ap, -1);
+        }
+    } while(tries < reconnect);
+    c->status = DEAD;
+    pthread_exit(&c->xreplica_id);
+    return 0;
+}
+
+int create_connection(struct node_io *n, int fd){
+    int rc = 0;
+    pthread_t id;
+    struct connection nns;
+    nns.chan_write = &n->chan_step;
+    nns.tid = &id;
+    nns.n = n;
+    nns.status = CONNECTING;
+    return pthread_create(&id, NULL, try_connect, &nns);
+}
+
+coroutine void node_listener(struct node_io *n){
+    int ls = listener(NODE_PORT);
+    if(ls <= 0) return;
+    while(n->running){
+        int fd = tcp_accept_raw(ls, 0, -1);
+        if(create_connection(n, fd) != 0){
+            msleep(now() + 200);
+        }
+    }
+}
+
+coroutine void client_listener(struct node_io *n){
+    int ls = listener(CLIENT_PORT);
+    if(ls <= 0) return;
+    while(n->running){
+        int chandle = tcp_accept(ls, 0, -1);
+        if(chandle <= 0){
+            msleep(now() + 60);
+            continue;
+        }
+    }
+}
+
+void *client_access(void *nn){
+    struct node_io *n = nn;
+    
+    bundle_go(n->ap, client_listener(n));
 }
 
 int start(struct node_io *n){
-    for(int i = 0;i < N; i++){
-       // set_con_state(&n->cons[i].fbs, n->r.id);
-        pthread_mutex_init(&n->cons[i].lock, NULL);
-        n->cons[i].ap = bundle();
-    }
     n->running = 1;
     n->ap = bundle();
     if(n->ap < 0) goto error;
@@ -103,220 +189,146 @@ void stop(struct node_io *n){
     hclose(n->ap);
 }
 
-int recv_io(struct node_io *n, struct message *m){
-    return chan_recv_spmc(&n->sync->chan_io, m);
+int write_step(struct connection *c, struct message *m){
+    return chan_send_mpsc(c->chan_write, m);
 }
 
-int recv_eo(struct node_io *n, struct message *m){
-    return chan_recv_spmc(&n->sync->chan_eo, m);
+int read_step(struct connection *c, struct message *m){
+    if(!chan_recv_spsc(&c->chan_read, &m)){
+        return -1;
+    }
+    return 0;
+}
+int write_propose(struct node_io *n, struct message *m){
+    return chan_send_mpsc(&n->sync->chan_propose, m);
 }
 
-int send_cont(struct node_io *n, struct message *m){
-    return chan_send_mpsc(&n->sync->chan_cont, m);
+int write_message(struct connection *c, struct message *m){
+    return msend(c->handle, m, sizeof(struct message), 40);
 }
 
-int send_new(struct node_io *n, struct message *m){
-    return chan_send_mpsc(&n->sync->chan_new, m);
-}
-
-int write_message(int ch, struct message *m){
-    return msend(ch, m, sizeof(struct message), 40);
-}
-
-ssize_t read_message(int sock, struct message *m){
-    m = malloc(sizeof(struct message));
-    if(m == 0){ errno = ENOMEM; return -1;}
+ssize_t read_message(struct connection *c, struct message *m){
     ssize_t s;
-    if((s = mrecv(sock, m, sizeof(struct message), -1 )) && (s >= 0)){
-        return s;
+    s = mrecv(c->handle, m, sizeof(struct message), 60);
+    if(s <= 0){
+        return -1;
     }
-    return -1;
+    //only alloc when we positively have a message
+    struct message *nm = malloc(sizeof(struct message));
+    if(nm == 0){ errno = ENOMEM; return -1;}
+    *nm = *m;
+    m = nm;
+    return s;
 }
 
-coroutine void internal_in(struct connection *c, struct node_io *n, int rep){
+coroutine void step_writer(struct connection *c){
     int rc = 0;
-    while(c->alive){
-        struct message *m;
-        ssize_t s = read_message(c->handle, m);
-        if(s <= 0) msleep(now() + 100);
-        if(m->start){
-            m->stop = now();
-            c->latency = m->start - m->stop;
+    struct message m;//temp storage for message to be received
+    struct message *rcv;
+    while(c->status == ALIVE){
+        memset(&m, 0, sizeof(struct message));
+        rcv = &m;
+        ssize_t s = read_message(c, rcv);
+        if(s <= 0){
+            msleep(now() + 20);
+            continue;
         }
-        send_cont(n, m);
-        rc = chsend(rep, &rc, sizeof(int), 20);
-        if(rc < 0) return;
+        rc = write_step(c, rcv);
+        if(rc <= 0) msleep(now() + 20);//TODO log lost messages.
     }
 }
 
-coroutine void internal_listen(struct node_io *n, int port, int rep){
-    int ls = listen(port);
-    int ch;
-    if(ls < 0) return;
-    while(n->running){
-        ch = tcp_accept(ls, NULL, -1);
-        if(ch < 0) return;
-        struct fbs_sock f;
-        //set_con_state(&f, n->r.id);
-        ch = fbs_sock_attach(ch, &f);
-        if(ch < 0) return;
-        struct connection *c = &n->cons[f.xreplica_id];
-        c->id = f.xreplica_id;
-        c->fbs = f;
-        c->handle = ch;
-        c->alive = 1;
-        bundle_go(c->ap, internal_in(c, n, rep));
-    }
-}
-
-//TODO check if override in protocol allows hclose on ssend routines
-// eg without closing the connection
-coroutine void try_send(struct node_io *n, struct message *m, int rep){
-    struct connection *c = &n->cons[m->to];
+coroutine void step_reader(struct connection *c){
     int rc = 0;
-    if(!c->alive){
-        rc = -1;
-        goto result;
-    }
-    rc = write_message(c->handle, m);
-    if(rc < 0){
-        rc = connect(rep, n, m->to);
-        if(rc < 0){
-            goto result;
+    struct message *m = 0;
+    while(c->status == ALIVE){
+        int s = read_step(c, m);
+        if(s <= 0){
+            msleep(now() + 20);
+            continue;
         }
+        rc = write_message(c, m);
+        if(rc <= 0) msleep(now() + 20);//TODO log lost messages.
     }
-result:
-    rc = chsend(rep, &rc, sizeof(int), 20);
 }
 
-coroutine void broadcast_prepare(struct node_io *n, struct message *m, int rep){
-    int rc = 0;
+int connection_write(struct node_io *n, struct message *m){
+    if(!chan_send_spsc(&n->chan_nodes[m->to].chan_read, m)){
+        //TODO raise error
+        return -1;
+    }
+    return 0;
+}
+
+void broadcast_prepare(struct node_io *n, struct message *m){
     int nN = N -1;
-    int ch[2];
-    rc = chmake(ch);
-    if(rc < 0){ goto report; }
-    size_t q = n->r.id;
-    for(int sending = 0;sending < nN;sending++){
+    size_t q = n->node_id;
+    for(int s = 0;s < nN;s++){
         q = (q+1) % N;
-        if(q == n->r.id) break;
+        if(q == n->node_id) break;
         m->to = q;
-        bundle_go(n->cons[q].sending, ssend(n, m, ch[1]));
+        connection_write(n, m);
     }
-    int result = 0;
-    int sent = 0;
-    int failed = 0;
-    while(sent < nN){
-        rc = chrecv(ch[0], &result, sizeof(int), 20);
-        if(rc < 0){ goto report; }
-        if(result >= 0) {
-            sent++;
-        } else {
-            failed++;
-        }
-        if(failed >= nN){ goto report; }
-    }
-    report(rep, &rc);
-report:
-    report(rep, &rc);
-    //TODO cancel routines when target reached
 }
 
-coroutine void broadcast_pre_accept(struct node_io *n, struct message *m, int rep){
-    int rc = 0;
+void broadcast_pre_accept(struct node_io *n, struct message *m){
     int nN = N -1;
-    int ch[2];
-    rc = chmake(ch);
-    if(rc < 0){ goto report; }
-    for(int sending = 0; sending < N -1; sending++){
-        m->to = n->quorum[sending];
-        bundle_go(n->cons[m->to].sending, ssend(n, m, ch[1]));
+    for(int s = 0; s < N -1; s++){
+        m->to = n->quorum[s];
+        connection_write(n, m);
     }
-    int result = 0;
-    int sent = 0;
-    int failed = 0;
-    while(sent < nN){
-        rc = chrecv(ch[0], &result, sizeof(int), 20);
-        if(rc < 0){ goto report; }
-        if(result >= 0){
-            sent++;
-        } else {
-            failed++;
-        }
-        if(failed >= nN){ goto report;}
-    }
-    report(rep, &rc);
-report:
-    report(rep, &rc);
-    //TODO cancel routines when target reached
 }
 
-coroutine void broadcast_try_pre_accept(struct node_io *n, struct message *m, int rep){
-    int rc = 0;
+void broadcast_try_pre_accept(struct node_io *n, struct message *m){
     int nN = N -1;
-    int ch[2];
-    rc = chmake(ch);
-    if(rc < 0) goto report;
-    for(int sending = 0; sending < N -1; sending++){
-        m->to = sending;
-        bundle_go(n->cons[sending].sending, ssend(n, m, ch[1]));
+    for(int s = 0; s < N -1; s++){
+        m->to = s;
+        connection_write(n, m);
     }
-    int result = 0;
-    int sent = 0;
-    while(sent < N){
-        rc = chrecv(ch[0], &result, sizeof(int), 20);
-        if(rc < 0) goto report;
-        sent++;
-    }
-    report(rep, &rc);
-report:
-    report(rep, &rc);
-
 }
 
-coroutine void broadcast_commit(struct node_io *n, struct message *m, int rep){
-    int rc = 0;
-    int ch[2];
-    rc = chmake(ch);
-    if(rc < 0) goto report;
-    for(int sending = 0; sending < N; sending++){
-        m->to = n->quorum[sending];
-        bundle_go(n->cons[m->to].sending, ssend(n, m, ch[1]));
+void broadcast_commit(struct node_io *n, struct message *m){
+    for(int s = 0; s < N; s++){
+        m->to = n->quorum[s];
+        connection_write(n, m);
     }
-    int result = 0;
-    int sent = 0;
-    while(sent < N){
-        rc = chrecv(ch[0], &result, sizeof(int), 20);
-        if(rc < 0) goto report;
-        sent++;
-    }
-report:
-    report(rep, &rc);
-
 }
 
-coroutine void internal_out(struct node_io *n, int rep){
-    struct message *m;
+int read_io(struct node_io *n, struct message *m){
+    if(!chan_recv_mpsc(&n->io_sync.chan_io, &m)){
+        return -1;
+    }
+    return 0;
+}
+
+//to other nodes
+coroutine void io_reader(struct node_io *n){
+    struct message *m = 0;
     while(n->running){
-        m = read_int(n);
-        m->from = n->r.id;
-        m->start = now();
+        if(read_io(n, m) <= 0){
+            msleep(now() + 20);
+            continue;
+        }
+        m->to = m->from;
+        m->from = n->node_id;
         switch(m->type){
             case PREPARE:
-                bundle_go(n->ap, broadcast_prepare(n, m, rep));
+                broadcast_prepare(n, m);
                 break;
             case ACCEPT:
             case PRE_ACCEPT:
-                update_quorum(n->quorum, n->cons);
-                bundle_go(n->ap, broadcast_pre_accept(n, m, rep));
+                update_quorum(n->quorum, n->chan_nodes);
+                broadcast_pre_accept(n, m);
                 break;
             case TRY_PRE_ACCEPT:
-                bundle_go(n->ap, broadcast_try_pre_accept(n, m, rep));
+                broadcast_try_pre_accept(n, m);
                 break;
             case COMMIT:
-                bundle_go(n->ap, broadcast_commit(n, m, rep));
+                broadcast_commit(n, m);
                 break;
             default:
-                bundle_go(n->ap, ssend(n, m, rep));
+                connection_write(n, m);
+                break;
 
         }
     }
--- src/io.h
+++ src/io.h
@@ -7,7 +7,9 @@
 
 #include <pthread.h>
 #include "fbs_sock.h"
-#include "node_sync.h"
+#include "sync.h"
+#include "message.h"
+#include "llrb-interval/llrb.h"
 
 enum protocol {
     TCP,
@@ -16,32 +18,55 @@ enum protocol {
 };
 
 struct connection {
-    size_t id;
+    volatile enum connection_status status;
+    size_t xreplica_id;
     int handle;
-    struct ipaddr addr;
+    pthread_t *tid;
+    struct node_io *n;
+    int fd;
     uint64_t latency;
+    struct ipaddr addr;
     int back_off;
     int error;
     enum protocol p;
     struct fbs_sock fbs;
     int ap;
-    int sending;
-    int alive;
-    pthread_mutex_t lock;
+    chan chan_read;
+    chan *chan_write;
+    pthread_rwlock_t lock;
 };
 
+struct client {
+    int handle;
+    int64_t expires;
+};
+
+struct registered_span {
+    char start_key[KEY_SIZE];
+    char end_key[KEY_SIZE];
+    char max[KEY_SIZE];
+    struct nxt next;
+    struct client clients[10];
+    LLRB_ENTRY(registered_span) entry;
+};
+
+LLRB_HEAD(client_tree, registered_span);
+LLRB_PROTOTYPE(client_tree, registered_span, entry, spcmp)
+
 struct node_io {
+    size_t node_id;
     int running;
     int ap;
-    struct sync *sync;
+    struct replica_sync sync[1];
+    struct io_sync io_sync;
+    chan chan_step;//IN steps for replica
+    chan chan_propose;//IN proposals for replica
+    chan chan_ctl;//IN ctl commands
+    struct connection chan_nodes[N];
+    struct span ranges[1];
     size_t quorum[N];
-    struct connection cons[N];
+    struct client_tree clients;
 };
 
 int start(struct node_io*);
 void stop(struct node_io*);
-int read_int(struct node_io*, struct message*);
-int write_int(struct node_io*, struct message*);
-int read_ext(struct node_io*, struct message*);
-int write_ext(struct node_io*, struct message*);
-int report(int, int*);
