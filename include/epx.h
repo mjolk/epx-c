b892470386c595fb3f668ddfb0c373c9e5d9127b
mjolk <2mjolk@gmail.com>
2019-03-27 21:58:04 +0100

    move env vars

--- include/epx.h
+++ include/epx.h
@@ -4,13 +4,16 @@
  * Author : Dries Pauwels <2mjolk@gmail.com>
  * Date   : Wed 05 Sep 2018 04:29
  */
-// the configured options and settings for epx
+// the compile time configured options and settings for epx
 #ifndef epx_h
 #define epx_h
 #define epx_VERSION_MAJOR 1
 #define epx_VERSION_MINOR 0
 #define N 3
 #define VALUE_SIZE 1024
-#define KEY_SIZE 3
+#define KEY_SIZE 3 //includes \0 terminator!
 #define MAX_DEPS 8
+#define TX_SIZE 100
+#define CLIENT_PORT 5552
+#define NODE_PORT 5551
 #endif
