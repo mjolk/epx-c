9f7999f3fdb58a099e15b2ad97f2455a081bbed3
mjolk <2mjolk@gmail.com>
2019-06-13 16:09:16 +0200

    format defines, format header

--- src/channel.c
+++ src/channel.c
@@ -10,15 +10,15 @@
 
 int chan_init(chan *c){
     ck_ring_init(&c->ring, CAPACITY);
-    #ifdef __APPLE__
+#ifdef __APPLE__
     c->c_sem = dispatch_semaphore_create(0);
     if(c->c_sem == NULL) goto init_error;
     c->s_sem = dispatch_semaphore_create(CAPACITY);
     if(c->s_sem == NULL) goto init_error;
-    #else
+#else
     if(sem_init(&c->c_sem, 0, 0) != 0) goto init_error;
     if(sem_init(&c->s_sem, 0, CAPACITY) != 0) goto init_error;
-    #endif
+#endif
     return 0;
 init_error:
     perror("init sync error");
@@ -43,20 +43,20 @@ int chan_recv_mpsc(chan *c, void *data){
 
 int sem_chan_send_mpsc(chan *c, void *data){
     int rc = 0;
-    #ifdef __APPLE__
+#ifdef __APPLE__
     dispatch_semaphore_wait(c->s_sem, DISPATCH_TIME_FOREVER);
-    #else
+#else
     do
         rc = sem_wait(&c->s_sem);
     while(rc != 0 && errno == EINTR);
     if(rc != 0 && errno != EINTR) goto send_error;
-    #endif
+#endif
     rc = ck_ring_enqueue_mpsc(&c->ring, c->b, data);
-    #ifdef __APPLE__
+#ifdef __APPLE__
     if(dispatch_semaphore_signal(c->c_sem) != 0) goto send_error;
-    #else
+#else
     if(sem_post(&c->c_sem) != 0) goto send_error;
-    #endif
+#endif
     return rc;
 send_error:
     perror("send sync error");
@@ -65,20 +65,20 @@ send_error:
 
 int sem_chan_recv_mpsc(chan *c, void *data){
     int rc = 0;
-    #ifdef __APPLE__
+#ifdef __APPLE__
     if(dispatch_semaphore_wait(c->c_sem, DISPATCH_TIME_FOREVER) != 0) goto recv_error;
-    #else
+#else
     do
         rc = sem_wait(&c->c_sem);
     while(rc != 0 && errno == EINTR);
     if(rc != 0 && errno != EINTR) goto recv_error;
-    #endif
+#endif
     rc = ck_ring_dequeue_mpsc(&c->ring, c->b, data);
-    #ifdef __APPLE__
+#ifdef __APPLE__
     if(dispatch_semaphore_signal(c->s_sem) < 0) goto recv_error;
-    #else
+#else
     if(sem_post(&c->s_sem) != 0) goto recv_error;
-    #endif
+#endif
     return rc;
 recv_error:
     perror("recv sync error");
--- src/channel.h
+++ src/channel.h
@@ -18,23 +18,28 @@
 typedef struct chan {
     ck_ring_t ring;
     ck_ring_buffer_t b[CAPACITY];
-    #ifdef __APPLE__
+#ifdef __APPLE__
     dispatch_semaphore_t c_sem;
     dispatch_semaphore_t s_sem;
-    #else
+#else
     sem_t c_sem;
     sema_t s_sem;
-    #endif
+#endif
 } chan;
 
 int chan_init(chan*);
+
 int chan_send_spmc(chan*, void*);
 int chan_recv_spmc(chan*, void*);
+
 int chan_send_mpsc(chan*, void*);
 int chan_recv_mpsc(chan*, void*);
+
 int chan_send_spsc(chan*, void*);
 int chan_recv_spsc(chan*, void*);
+
 int chan_send_mpmc(chan*, void*);
 int chan_recv_mpmc(chan*, void*);
+
 int sem_chan_send_mpsc(chan*, void*);
 int sem_chan_recv_mpsc(chan*, void*);
